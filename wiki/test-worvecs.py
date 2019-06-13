e60af210cca5ce795c45ddac30ca49c2d4d72a03
Max B <mxbnai@gmail.com>
2019-06-13 17:40:48 +1000

    wiki worvecs test

--- wiki/test-worvecs.py
+++ wiki/test-worvecs.py
@@ -7,7 +7,7 @@ model = worvecs()
 model.load('./data/wiki-worvecs-0.txt.gz')
 
 print('\nking -> queen, father -> ?:')
-for s in model.similarRelations('king', 'queen', 'father')[0]:
+for s in model.similarRelations('car', 'driver', 'motorcycle')[0]:
     print('\t%s' % s)
 
 print('\nbus:')
